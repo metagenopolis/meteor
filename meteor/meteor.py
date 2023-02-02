28b08ac55b3686ceee89ce0e9ac82321a7797466
Amine  GHOZLANE <amine.ghozlane@pasteur.fr>
2023-02-02 18:17:04 +0100

    default factory

--- meteor/meteor.py
+++ meteor/meteor.py
@@ -242,7 +242,7 @@ class MeteorMapper:
 class MeteorSession:
     logger: logging.Logger
     tmp_path: str
-    FMeteorJobIniFile: ConfigParser = ield(default_factory=ConfigParser)
+    FMeteorJobIniFile: ConfigParser = field(default_factory=ConfigParser)
     FMeteorJobIniFilename: str = "" 
     FProjectDir: str = ""
     FSampleDir: str = ""
