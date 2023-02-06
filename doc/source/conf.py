325d4f283e6e2fc74c04a367b69953e0a9812ff4
GHOZLANE AMINE <amine.ghozlane@inrae.fr>
2023-02-06 16:23:56 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,9 +6,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import pathlib
+import shutil
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
+
 project = 'Meteor'
 copyright = '2022, INRAE'
-author = 'Franck Gauthier, Nicolas Pons, Amine Ghozlane'
+author = 'Franck Gauthier, Nicolas Pons'
 release = '3.3'
 
 # -- General configuration ---------------------------------------------------
@@ -26,3 +33,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# The readme that already exists
+readme_path = pathlib.Path(__file__).parent.resolve().parent / "README.rst"
+# We copy a modified version here
+readme_target = pathlib.Path(__file__).parent / source / "README.rst"
+shutil.copyfile(readme_path, readme_target)
