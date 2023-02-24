d5fcff840a76fd4c610b8118821bd338fdfe8719
Amine  GHOZLANE <amine.ghozlane@pasteur.fr>
2023-02-24 15:51:07 +0100

    Clean code

--- meteor/tests/test_reference_builder.py
+++ meteor/tests/test_reference_builder.py
@@ -1,36 +1,29 @@
 from ..session import Component
+from typing import Type
 from ..referencebuilder import ReferenceBuilder
 from pathlib import Path
 from hashlib import md5
 import pytest
-import shutil
-
-@pytest.fixture
-def builder_defec(tmpdir, datadir):
-    return  ReferenceBuilder(Component(ref_dir=Path(tmpdir)), input_fasta = datadir/ "defect_catalogue.fasta.gz")
 
 
 @pytest.fixture
-def builder(tmpdir, datadir):
-    return  ReferenceBuilder(Component(ref_dir=Path(tmpdir), ref_name="test"), input_fasta = datadir/ "test_catalogue.fasta.gz")
+def builder_defec(tmp_path: Path, datadir: Path):
+    meteor = Component
+    meteor.ref_dir=tmp_path
+    meteor.ref_name="defect"
+    return ReferenceBuilder(meteor, input_fasta=datadir/"defect_catalogue.fasta.gz")
 
-# def get_read_reference(builder_defec):
-#     yield from builder_defec.read_reference()
 
-# @pytest.fixture(params=get_read_reference())
-# def line_fixture(request):
-#     return request.param
+@pytest.fixture
+def builder(tmp_path: Path, datadir: Path):
+    meteor = Component
+    meteor.ref_dir=tmp_path
+    meteor.ref_name="test"
+    return ReferenceBuilder(meteor, input_fasta=datadir/"test_catalogue.fasta.gz")
 
-# @pytest.mark.parametrize(
-#     ("length", "sequence"),
-#     (
-#         (80, "ATGAAAATGAACCTGCAAAAGGACATGTTTGATCGCAAATTGCGATACAAGATGTACAAAGATGGTAAAAAGTGGGTGTT"),
-#         (56, "TGCCAGCATGGCGACTTTGTCTTTGATTGGGGCTTTTTTGGGCGGTGGTAGCGCCC"),
-#         (74, "ATGAAAATGAACCTGCAAAAGGACGATCGCAAATTGCGATACAAGATGTACAAAGATGGTAAAAAGTGGGTGTT"),
-#     ),
-# )
-def test_read_reference(builder_defec):
-    res =  (
+
+def test_read_reference(builder_defec: ReferenceBuilder):
+    res = (
         (80, "ATGAAAATGAACCTGCAAAAGGACATGTTTGATCGCAAATTGCGATACAAGATGTACAAAGATGGTAAAAAGTGGGTGTT"),
         (56, "TGCCAGCATGGCGACTTTGTCTTTGATTGGGGCTTTTTTGGGCGGTGGTAGCGCCC"),
         (74, "ATGAAAATGAACCTGCAAAAGGACGATCGCAAATTGCGATACAAGATGTACAAAGATGGTAAAAAGTGGGTGTT"),
@@ -39,18 +32,16 @@ def test_read_reference(builder_defec):
         assert length == res[i][0]
         assert sequence == res[i][1]
 
+
 @pytest.mark.parametrize(
     ("annotation_md5", "fasta_md5"),
     (
         pytest.param("b8d7e70fe88abbf1cf1746e5a02439ea", "2912b682a8e7554025cc5feadd641570", id="Accurate output"),
     )
 )
-def test_create_reference(builder, annotation_md5, fasta_md5):
+def test_create_reference(builder, annotation_md5: str, fasta_md5: str):
     builder.create_reference()
     with open(builder.output_annotation_file, "rb") as output_annotation_file:
         assert md5(output_annotation_file.read()).hexdigest() == annotation_md5
     with open(builder.output_fasta_file, "rb") as output_fasta_file:
         assert md5(output_fasta_file.read()).hexdigest() == fasta_md5
-
-    # with pytest.raises(ValueError):
-    #     Line that raise a value error
