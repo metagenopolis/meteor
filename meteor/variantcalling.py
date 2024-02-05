6a92808ae4902461c92b951cd2c4ea41744726a6
Amine  GHOZLANE <amine.ghozlane@pasteur.fr>
2024-02-05 20:53:43 +0100

    Remove bedtools dependency

--- meteor/variantcalling.py
+++ meteor/variantcalling.py
@@ -17,13 +17,14 @@ from dataclasses import dataclass
 from pathlib import Path
 from configparser import ConfigParser
 from datetime import datetime
-from typing import Type
+from typing import Type, Dict
 from meteor.session import Session, Component
 from time import perf_counter
 import tempfile
 from tempfile import NamedTemporaryFile
 from packaging.version import Version, parse
-from io import StringIO
+from pysam import AlignmentFile
+from collections import defaultdict
 import pandas as pd
 import logging
 import sys
@@ -45,7 +46,6 @@ class VariantCalling(Session):
         vcf_file: Path,
         consensus_file: Path,
         bcftool_version: str,
-        bedtools_version: str,
     ) -> ConfigParser:  # pragma: no cover
         """Define the census 1 configuration
 
@@ -65,8 +65,6 @@ class VariantCalling(Session):
         config["variant_calling"] = {
             "variant_calling_tool": "bcftools",
             "variant_calling_version": bcftool_version,
-            "bed_manipulation": "bedtools",
-            "bedtools_version": bedtools_version,
             "variant_calling_date": datetime.now().strftime("%Y-%m-%d"),
             "vcf_name": vcf_file.name,
             "consensus_name": consensus_file.name,
@@ -76,20 +74,114 @@ class VariantCalling(Session):
         }
         return config
 
+    def get_regions(self, gene_name: str, reads_dict: Dict[int, int]) -> pd.DataFrame:
+        dico_inverse = defaultdict(list)
+        for position, count in reads_dict.items():
+            dico_inverse[count].append(position)
+
+        dico_regions = {}
+        for count, positions in dico_inverse.items():
+            positions.sort()
+            region = []
+            debut, fin = positions[0], positions[0]
+
+            for pos in positions[1:]:
+                if pos == fin + 1:
+                    fin = pos
+                else:
+                    region.append([debut, fin + 1])
+                    debut = fin = pos
+            region.append([debut, fin + 1])
+            dico_regions[count] = region
+        data = []
+        for count, regions in dico_regions.items():
+            for region in regions:
+                data.append((gene_name, region[0], region[1], count))
+        return pd.DataFrame(
+            data,
+            columns=["gene_id", "startpos", "endpos", "coverage"],
+        )
+
+    def count_reads_in_gene(
+        self, cram, gene_name: str, gene_length: int, reads_dict: Dict[int, int]
+    ):
+        """
+        Counts the number of reads at each position in a specified gene from a BAM file,
+        ignoring reads with gaps or deletions at that position.
+
+        Parameters:
+        bam (pysam.AlignmentFile): A pysam AlignmentFile object representing the BAM file
+        gene_name (str): The name of the gene for which reads are to be counted
+
+        Returns:
+        dict: A dictionary with positions (int) as keys and the number of reads (int) at each position as values.
+        """
+        # gene_length = bam.lengths[bam.references.index(gene_name)]
+        # Extraction of positions having at least one read
+        for pileupcolumn in cram.pileup(
+            contig=gene_name,
+            start=0,
+            end=gene_length,
+            stepper="all",
+            max_depth=self.max_depth,
+        ):
+            read_count = sum(
+                1
+                for pileupread in pileupcolumn.pileups
+                if not pileupread.is_del and not pileupread.is_refskip
+            )
+            reads_dict[pileupcolumn.reference_pos] = read_count
+        # Add positions without reads
+        for position in range(gene_length):
+            if position not in reads_dict:
+                reads_dict[position] = 0
+
+        return reads_dict
+
     def filter_low_cov_sites(
-        self, output: str, temp_low_cov_sites: tempfile._TemporaryFileWrapper
-    ) -> None:
-        """Write sites with a low coverage"""
-        sum_cov_bed = pd.read_csv(
-            StringIO(output),
+        self, cram_file: Path, temp_low_cov_sites: tempfile._TemporaryFileWrapper
+    ):
+        # Get list of genes
+        gene_interest = pd.read_csv(
+            self.matrix_file,
             sep="\t",
             names=[
                 "gene_id",
-                "startpos",
-                "endpos",
+                "gene_length",
                 "coverage",
             ],
+            header=1,
         )
+        # We need to do more work for these genes
+        # their total count is above the min_snp_depth
+        # but we need to find on which positions they do.
+        gene_tofilter = gene_interest[gene_interest["coverage"] >= self.min_snp_depth][
+            ["gene_id", "gene_length"]
+        ]
+        reads_dict = defaultdict(int)
+        dfs = []
+        with AlignmentFile(cram_file, "rb") as cram:
+            for index, row in gene_tofilter.iterrows():
+                reads_dict = self.count_reads_in_gene(
+                    cram, str(row["gene_id"]), row["gene_length"], reads_dict
+                )
+                df = self.get_regions(str(row["gene_id"]), reads_dict)
+                dfs.append(df)
+        # All these genes are going to be replaced by gaps
+        # Their count is below the threshold level
+        gene_ignore = gene_interest[gene_interest["coverage"] < self.min_snp_depth]
+        dfs.append(
+            pd.DataFrame(
+                {
+                    "gene_id": gene_ignore["gene_id"],
+                    "startpos": 0,
+                    "gene_length": gene_ignore["gene_length"],
+                    "coverage": gene_ignore["coverage"],
+                }
+            )
+        )
+        sum_cov_bed = pd.concat(dfs, ignore_index=True)
+        #
         sum_cov_bed.query(f"coverage < {self.min_snp_depth}").to_csv(
             temp_low_cov_sites, sep="\t", header=False, index=False
         )
@@ -100,6 +192,10 @@ class VariantCalling(Session):
         cram_file = (
             self.census["mapped_sample_dir"]
             / f"{self.census['census']['sample_info']['sample_name']}.cram"
+        )
+        self.matrix_file = (
+            self.census["mapped_sample_dir"]
+            / f"{self.census['census']['sample_info']['sample_name']}.tsv"
         )
         vcf_file = (
             self.census["directory"]
@@ -132,19 +228,6 @@ class VariantCalling(Session):
             logging.error(
                 "Error, the bcftools version %s is outdated for meteor. Please update bcftools to >= 0.1.19.",
                 bcftools_version,
-            )
-            sys.exit()
-        bedtools_exec = run(["bedtools", "--version"], capture_output=True)
-        bedtools_version = bedtools_exec.stdout.decode("utf-8").split(" ")[1][1:]
-        if bedtools_exec.returncode != 0:
-            logging.error(
-                "Check bedtools failed:\n%s" % bedtools_exec.stderr.decode("utf-8")
-            )
-            sys.exit()
-        elif parse(bedtools_version) < Version("2.18"):
-            logging.error(
-                "Error, the bedtools version %s is outdated for meteor. Please update bedtools to >= 2.18.",
-                bedtools_version,
             )
             sys.exit()
         start = perf_counter()
@@ -217,17 +300,7 @@ class VariantCalling(Session):
                 with NamedTemporaryFile(
                     mode="wt", dir=self.meteor.tmp_dir, delete=False, suffix=".bed"
                 ) as temp_low_cov_sites:
-                    output = run(
-                        [
-                            "bedtools",
-                            "genomecov",
-                            "-bga",
-                            "-icram",
-                            str(cram_file.resolve()),
-                        ],
-                        capture_output=True,
-                    ).stdout.decode("utf-8")
-                    self.filter_low_cov_sites(output, temp_low_cov_sites)
+                    self.filter_low_cov_sites(cram_file, temp_low_cov_sites)
                     check_call(
                         [
                             "bcftools",
@@ -245,7 +318,7 @@ class VariantCalling(Session):
                     )
         logging.info("Completed SNP calling in %f seconds", perf_counter() - start)
         config = self.set_variantcalling_config(
-            cram_file, vcf_file, consensus_file, bcftools_version, bedtools_version
+            cram_file, vcf_file, consensus_file, bcftools_version
         )
         self.save_config(config, self.census["Stage3FileName"])
         return True
