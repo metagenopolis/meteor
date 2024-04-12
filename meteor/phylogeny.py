5a9b2174abd9446ee466bb9e77987e23438220b2
Amine  GHOZLANE <amine.ghozlane@pasteur.fr>
2024-04-12 10:45:15 +0200

    Fix format issues with cogent and raxml-ng refusing to go further

--- meteor/phylogeny.py
+++ meteor/phylogeny.py
@@ -97,6 +97,12 @@ class Phylogeny(Session):
         }
         return config
 
+    def remove_edge_labels(self, newick: str) -> str:
+        # This regular expression matches the edge labels (like "edge.0:")
+        pattern = r"\b(edge\.\d+):\b"
+        # Replace matched patterns with ":" (effectively removing the edge label)
+        return re.sub(pattern, ":", newick)
+
     def execute(self) -> None:
         logging.info("Launch phylogeny analysis")
         # Define the regex pattern to match the version number
@@ -145,6 +151,7 @@ class Phylogeny(Session):
                             "--model",
                             "GTR+G",
                             "--redo",
+                            # "--force perf_threads", # only with raxml-ng-mpi
                             "--prefix",
                             str(tree_file.resolve()),
                         ]
@@ -160,11 +167,21 @@ class Phylogeny(Session):
                         temp_clean.name,
                         moltype="dna",
                     )
+                    aligned_seqs.quick_tree()
                     d = EstimateDistances(aligned_seqs, submodel=GTR())
                     d.run(show_progress=False)
                     mycluster = upgma(d.get_pairwise_distances())
                     mycluster = mycluster.unrooted_deepcopy()
-                    mycluster.write(tree_file.with_suffix(".tree"))
+                    with tree_file.with_suffix(".tree").open("w") as f:
+                        f.write(
+                            self.remove_edge_labels(
+                                mycluster.get_newick(with_distances=True)
+                            )
+                        )
+                    # Add egde.x which is not accepted by ete3
+                    # mycluster.write(
+                    #     tree_file.with_suffix(".tree"),
+                    # )
                 tree_files.append(tree_file)
             logging.info("Completed MSP tree %d/%d", idx, msp_count)
         logging.info("Completed phylogeny in %f seconds", perf_counter() - start)
