930b2434de5b632cd109fdbe7e636622cd173557
Amine Ghozlane <amine.ghozlane@pasteur.fr>
2024-09-02 20:01:34 +0200

    Freebayes minimum version must be 1.3.6

--- meteor/session.py
+++ meteor/session.py
@@ -28,7 +28,7 @@ class Component:
     """Set of important constant for meteor"""
 
     MIN_BOWTIE2_VERSION: ClassVar[Version] = Version("2.3.5")
-    MIN_FREEBAYES_VERSION: ClassVar[Version] = Version("1.2")
+    MIN_FREEBAYES_VERSION: ClassVar[Version] = Version("1.3.6")
     DEFAULT_GAP_CHAR: ClassVar[str] = "N"
     DEFAULT_CORE_SIZE: ClassVar[int] = 100
 
